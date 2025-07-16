67a9fc2b3e54bf1cdcb6f97048d24674c06bcbc2
Marley Alford <marleycodes@gmail.com>
2025-07-16 23:30:21 +0200

    Refactored staff data to use JSON

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Contact from "../components/Contact";
 import Section from "../components/Section";
 import StaffComponent from "../components/StaffComponent";
-import staffData from "../data/staff";
+import staffDataJSON from "../data/staff.json";
 
 function About() {
   let [userEmail, setUserEmail] = useState("");
+  let [staffData, setStaffData] = useState([]);
+
+  useEffect(() => {
+    setStaffData(staffDataJSON);
+  });
 
   function handleChange(e) {
     setUserEmail(e.target.value);
@@ -15,6 +20,17 @@ function About() {
     alert(`Do you want to subscribe with this email?: ${userEmail}`);
   }
 
+  let staffJSXArray = staffData.map((staffMember) => {
+    return (
+      <StaffComponent
+        key={staffMember.id}
+        filePath={staffMember.imgFilePath}
+        name={staffMember.name}
+        bio={staffMember.bio}
+      />
+    );
+  });
+
   return (
     <div className="page-container">
       <Section
@@ -22,23 +38,7 @@ function About() {
         text="Marley's Cafe began in 2023 with a dream and a GoFundMe. It turns out that a lot of people want to experience the feeling of drinking coffee while surrounded by llamas! We are currently the first and only llama cafe in all of Germany. The llamas are all rescues."
       />
       <div className="container">
-        <div className="row">
-          <StaffComponent
-            filePath={staffData[0].imgFilePath}
-            name={staffData[0].name}
-            bio={staffData[0].bio}
-          />
-          <StaffComponent
-            filePath={staffData[1].imgFilePath}
-            name={staffData[1].name}
-            bio={staffData[1].bio}
-          />
-          <StaffComponent
-            filePath={staffData[2].imgFilePath}
-            name={staffData[2].name}
-            bio={staffData[2].bio}
-          />
-        </div>
+        <div className="row">{staffJSXArray}</div>
       </div>
 
       <Contact />
